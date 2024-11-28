f98814a54ec39eb9ad9722a72a99cdc142a7d96b
Abdurahmon <abdurahmonikromov634@gmail.com>
2024-11-28 19:24:14 +0500

    exam answers

---
+++ main.js
@@ -0,0 +1,47 @@
+// 5.
+// let speed = prompt("Tezlikni kiriting")
+// let score = (speed - 70) / 5
+
+// if(speed < 70) {
+//     console.log("Hammasi yaxshi");
+// }
+// else if(score > 12) {
+//     console.log('Prava olindi!');
+// }
+// else if(score < 12) {
+//     console.log(`${score} points`);
+// }
+
+
+// 6.
+// let number1 = prompt("1-son kiriting") - 0
+// let number2 = prompt("2-son kiriting") - 0
+// let number3 = prompt("3-son kiriting") - 0
+// let arr = [number1, number2, number3]
+// let total = 0
+// for(let i = 0; i < arr.length; i++) {
+//     total = total + arr[i]
+// }
+// let result = total / arr.length
+// console.log(result);
+
+
+// 7.
+// let fruits = ['apple', 'banana', 'orange']
+// let fruitName = prompt("Meva nomini kiriting").trim().toLowerCase()
+// let result = fruits.some(value => fruitName == value)
+// if(String(result) == 'true') {
+//     console.log('Bu meva mavjud');
+// }
+// else{
+//     console.log('Bu meva mavjud emas');
+// }
+
+
+// 8.
+// let arr = [1,3,2,4,5,6,7,8,9]
+// let total = 0
+// for(let i = 0; i < arr.length; i++) {
+//     total = total + arr[i]
+// }
+// console.log(total);
